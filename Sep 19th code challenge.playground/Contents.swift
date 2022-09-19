ef9b5a25db8e14c8230dcf4d38a40575df7f6bb0
Easton Butterfield <eastonbutterfield@Eastons-MacBook-Air.local>
2022-09-19 07:50:12 -0600

    added sep 19th code challenge

---
+++ Sep 19th code challenge.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+func findCentury(year: Double){
+    let years: Double = year / 100
+    if years.truncatingRemainder(dividingBy: 1) == 0 {
+        print(years)
+    } else {
+        print (Int(years + 1))
+    }
+}
+findCentury(year: 1601)
