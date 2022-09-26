9863ea7bd817f5ee82464d08e27f8e6bc1d9290f
Easton Butterfield <eastonbutterfield@Eastons-MacBook-Air.local>
2022-09-26 09:31:12 -0600

    added calculator update

--- Constraints practice/Constraints practice/ViewController.swift
+++ Constraints practice/Constraints practice/ViewController.swift
@@ -6,14 +6,39 @@
 //
 
 import UIKit
+import RyansPokemonPackage
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var newPokemonButton: UIButton!
+    @IBOutlet weak var pokemonNameLable: UILabel!
+    @IBOutlet weak var pokemonImageView: UIImageView!
+    
+    @IBOutlet weak var typeStackView: UIStackView!
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        newPokemonButton.layer.cornerRadius = newPokemonButton.frame.height / 2
     }
 
-
+    @IBAction func newPokemonButtonTapped(_ sender: Any) {
+        Task {
+            let pokemon = await getRandomPokemon(gen: .all)
+            pokemonNameLable.text = pokemon.name
+            pokemonImageView.setPokemon(pokemon: pokemon)
+            for view in typeStackView.arrangedSubviews {
+                view.removeFromSuperview()
+            }
+            for pType in pokemon.types {
+                let lable = UILabel()
+                lable.text = pType
+                lable.text = pType.capitalized
+                lable.textAlignment = .center
+                typeStackView.addArrangedSubview(lable)
+            }
+        }
+    }
+    
 }
 
